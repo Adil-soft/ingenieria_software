a0ab22b2869f23b035d1e19007ef777fa1381a1e
Adil <adil.infosoft@gmail.com>
2020-09-22 20:22:30 +0200

    now with strcat

---
+++ include/myinclude.h
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "string.h" 
+
+
+void f(){
+
+        char hello[]="hello world," ;
+        char myname[]=" my name is Adil.";
+        printf("%s\n", strcat(hello,myname));
+}
