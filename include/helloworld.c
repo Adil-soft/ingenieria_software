c30c6c2719fbee04d7fffc207c1d09cc483348ac
Adil <adil.infosoft@gmail.com>
2020-09-22 21:03:59 +0200

    changing..

--- include/helloworld.c
+++ include/helloworld.c
@@ -1,6 +1,6 @@
 /***********************************************************************************************************************
 
-        author : Adil, el guerouj  hirchi 
+        author : Adil, changing
         date : 22 sept 2020
         profesora : Aurora Ramirez 
         asignatura : ingenieria de software 
